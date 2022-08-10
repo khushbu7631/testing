2fbe6f1dea8f4b6851a0d44946a38074af17872e
Khushbu <Khushbusingh786khsingh.com>
2022-08-10 12:06:30 +0530

    primary change

--- _search.php
+++ _search.php
@@ -22,7 +22,7 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'created_by') ?>
 
     <div class="form-group">
-        <?= Html::submitButton('Search', ['class' => 'btn btn-danger']) ?>
+        <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
         <?= Html::resetButton('Reset', ['class' => 'btn btn-outline-secondary']) ?>
     </div>
 
